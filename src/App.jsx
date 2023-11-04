9d2500a63f0956623d0e88a302497251e438dcee
ankit1519 <raazankit1502@gmail.com>
2023-11-04 08:59:20 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import Video from "./components/Video.jsx";
+function App() {
+  return (
+    <div className="App">
+      <Video
+        thumbnail="https://images.pexels.com/photos/270557/pexels-photo-270557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title="react js tut"
+        views="10k"
+        time="30 min ago"
+        channel="code n build with ankit"
+      />
+      <Video
+        thumbnail="https://images.pexels.com/photos/1181298/pexels-photo-1181298.jpeg?auto=compress&cs=tinysrgb&w=600"
+        title="js tut"
+        views="100k"
+        time="1 year ago"
+        channel="programmer ji"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Video.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Video.css'
+function Video({thumbnail,channel,title,views,time}) {
+  return (
+   <>
+   <div className="container">
+    <div className="pic">
+        <img src={thumbnail} alt="" />
+    </div>
+    <div className="title">{title}</div>
+    <div className="chaneel">{channel}</div>
+    <div className="views">{views} views <span>.</span>{time}</div>
+   </div>
+   </>
+  )
+}
+
+export default Video
